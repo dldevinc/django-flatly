1989e8542245ac2dff562b8731c4659eb2a55f65
pix <pix666@ya.ru>
2020-07-11 01:05:39 +0400

    upgrade env

--- tests/settings.py
+++ tests/settings.py
@@ -68,7 +68,7 @@ WSGI_APPLICATION = 'wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'NAME': 'tests/db.sqlite3',
     }
 }
 
